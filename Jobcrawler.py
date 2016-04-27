90d005ce35f6cdefc859827f62b887048f6160df
Roni Rengit <RoniRengit@users.noreply.github.com>
2016-04-27 16:57:37 +0530

    Added files via upload
    Now you can choose where to save the file and the name of the file. By default, the file will be saved in the current working directory.

--- Jobcrawler.py
+++ Jobcrawler.py
@@ -5,18 +5,18 @@ installed. You will have to import BeautifulSoup and re modules. You can type
 "pip install bs4" and "pip install re" respectively in the command prompt and
 python would automatically download them.
 
-The jobs are posted in a doc format on the desktop. You could change path by
-editing file_locationa and filename.
+The jobs are posted in a doc format on the desktop. 
 
 The crawler parses Job Title, Company Name and the job link.
 Any questions, you can email me at sharpeyekool@gmail.com
 --------------------------------------------------------------------------------------------------------
 Created by Roni Rengit"""
 
-#!/usr/local/bin/python3
+#!/usr/local/bin/python
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
+import os
 
 #Console
 print("Webcrawler for www.technopark.org job site")
@@ -31,15 +31,21 @@ else:
   print("Sorry, I have not checked the program for non-Windows systems\n")
 
 #Open file
-file_location=r'C:\Users\ferdinand-pc\Desktop\\'
-filename='Yellow Pages.doc' # Windows 98 to Windows 10 compatible
+option =input("Ths site has been scrapped.The file will be saved with a default name in the current directory. Do you want to change it? ('Y' or 'N'): ")
+
+if (option=='Y') or (option=='y'):
+  file_location=input("Enter URL: ")
+  filename=input("File Name: ")
+else:
+  file_location=os.getcwd()
+  filename="TP Joblisting"
+
 try:
-  file=open(file_location+filename, 'w', encoding='utf-8')
-except PermissionError:
+  file=open(file_location+"\\"+filename+'.doc', 'w', encoding='utf-8')
+except:
   print("Please close the document and try again")
   input("")
-  sys.exit()
-
+  
 #Parsing function
 def parser(soup_html):
     JobTitle=soup.tbody.findAll("a", {"class":"jobTitleLink"})
@@ -64,22 +70,23 @@ def parser(soup_html):
     for  i in range(len(jobslist)):
 
       #Uncomment below to see job postings on console
-      print('Job Title:'+jobslist[i])
+      """print('Job Title:'+jobslist[i])
       print('Company Name: '+companieslist[i])
-      print("Links: "+r"http://www.technopark.org/job-search"+linkslist[i]+"\n")
+      print("Links: "+r"http://www.technopark.org/job-search"+linkslist[i]+"\n")"""
       
       file.write("Job Title: "+jobslist[i]+"\n") 
       file.write("Company Name: "+companieslist[i]+"\n")
       file.write(r"http://www.technopark.org/"+linkslist[i]+"\n")
       file.write("\n")
-    print("Done. Please check "+file_location+filename)
+    print("Done. Please check "+file_location+"\\"+filename+'.doc')
+    print("Thank you")
     file.close()
       
 #Checks for any errors due to site issues such as web page not found or server issues, etc
 try:
   URL=r"http://www.technopark.org/job-search"
   URL_test=r"File:\C:\Users\ferdinand-pc\Desktop\technopark.html"
-  html=urlopen(URL, {"headers"})
+  html=urlopen(URL)
   status="Successful"
 except:
   print("Could not open website. Check for connectivity issues.\n")
@@ -91,5 +98,5 @@ if(status=='Successful'):
   parser(soup)
 else:
   print("Try again")
-
+  
 input("")
